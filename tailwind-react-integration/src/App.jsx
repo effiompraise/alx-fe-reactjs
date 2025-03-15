1f6e16f57deb5989c837cd99e8c4a06db6d581a0
Praise_Effiom <effiompraise199@gmail.com>
2025-03-15 10:52:09 -0400

    Created a components folder and moved the UserProfile.jsx file there

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import UserProfile from './UserProfile'
+import UserProfile from './components/UserProfile'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
